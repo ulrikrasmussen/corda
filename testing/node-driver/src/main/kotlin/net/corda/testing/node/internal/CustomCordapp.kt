c46d99f41c3a3d9a161ffe228cfb37e9e26f5314
Chris Rankin <chris.rankin@r3.com>
2021-11-23 09:43:41 +0000

    ENT-6412: Upgrade ClassGraph 4.8.90 -> 4.8.135. (#6973)

--- testing/node-driver/src/main/kotlin/net/corda/testing/node/internal/CustomCordapp.kt
+++ testing/node-driver/src/main/kotlin/net/corda/testing/node/internal/CustomCordapp.kt
@@ -64,6 +64,12 @@ data class CustomCordapp(
         }
 
         classGraph.enableClassInfo().pooledScan().use { scanResult ->
+            if (scanResult.allResources.isEmpty()) {
+                throw ClassNotFoundException(
+                    "Could not create jar file as the given classes(${classes.joinToString()}) / packages(${packages.joinToString()}) were not found on the classpath"
+                )
+            }
+
             val whitelistService = SerializationWhitelist::class.java.name
             val whitelists = scanResult.getClassesImplementing(whitelistService)
 
@@ -80,7 +86,7 @@ data class CustomCordapp(
 
                 // The same resource may be found in different locations (this will happen when running from gradle) so just
                 // pick the first one found.
-                scanResult.allResources.asMap().forEach { path, resourceList ->
+                scanResult.allResourcesAsMap.forEach { (path, resourceList) ->
                     jos.addEntry(testEntry(path), resourceList[0].open())
                 }
             }
